8919848377174a5c0c3d351fa28c79860e8fbbfe
Кристина <m_krissstina@mail.ru>
2021-12-19 20:56:17 +0300

    add

--- Makefile
+++ Makefile
@@ -13,8 +13,9 @@ SRC_PATH_TASK_2_2 = hw_2.2/dprintf.c
 SRC_PATH_TASK_3 = hw_3/copy.c
 SRC_PATH_TASK_6_1 = hw_6.1/dirent.c
 SRC_PATH_TASK_7_1 = hw_7.1/copydir.c
+SRC_PATH_TASK_9 = hw_9/statvfs.c
 
-all: hw_1 hw_2.1 hw_2.2 hw_3 hw_6.1 hw_7.1
+all: hw_1 hw_2.1 hw_2.2 hw_3 hw_6.1 hw_7.1 hw_9
 
 hw_1:   ${SRC_PATH_TASK_1} working_with_files_lib.c 
 	$(C) -o out ${SRC_PATH_TASK_1} working_with_files_lib.c
@@ -28,6 +29,8 @@ hw_6.1: ${SRC_PATH_TASK_6_1} working_with_files_lib.c
 	$(C) -o out ${SRC_PATH_TASK_6_1} working_with_files_lib.c 
 hw_7.1: ${SRC_PATH_TASK_7_1} working_with_files_lib.c 
 	$(C) -o out ${SRC_PATH_TASK_7_1} working_with_files_lib.c 
+hw_9: ${SRC_PATH_TASK_9}
+	$(C) -o out ${SRC_PATH_TASK_9} 
 
 
 clear:
---
+++ hw_9/statvfs.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <sys/statvfs.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+void system_file_info(const char *fs_name, struct statvfs *sbfs) {
+    printf("Usage: %s\n\n", fs_name);
+
+    printf("Filesystem block size:                          %lu\n", sbfs->f_bsize);
+    printf("Fragment size:                                  %lu\n", sbfs->f_frsize);
+    printf("Size of filesystem in fragment size units:      %lu\n", sbfs->f_blocks);
+    printf("Number of free blocks:                          %lu\n", sbfs->f_bfree);
+    printf("Number of free blocks for unprivileged users:   %lu\n", sbfs->f_bavail);
+    printf("Number of inodes:                               %lu\n", sbfs->f_files);
+    printf("Number of free inodes:                          %lu\n", sbfs->f_ffree);
+    printf("Number of free inodes for unprivileged users:   %lu\n", sbfs->f_favail);
+    printf("Filesystem ID:                                  %lu\n", sbfs->f_fsid);
+    printf("Mount flags:                                    %lu\n", sbfs->f_flag);
+    printf("Maximum filename length:                        %lu\n", sbfs->f_namemax);
+}
+
+ int main(int argc, char *argv[]) {
+     struct statvfs sbfs;
+     struct stat sb;
+
+     char *file_name;
+     char *fs_name;
+
+     if (argc < 2) {
+         fprintf(stderr, "Expected more argunemts. For example: ./a.out  file_system_name  file_name\n");
+         return 0;
+     }
+     else if (argc == 2) {
+         file_name = (char*)malloc(strlen(argv[1]));
+         strcpy(file_name, argv[1]);
+     }
+     else {
+        fs_name = (char*)malloc(strlen(argv[1]) + sizeof("."));
+        strcpy(fs_name, ".");
+        strcat(fs_name, argv[1]);
+        file_name = (char*)malloc(strlen(argv[2]));
+     } 
+     
+     if (lstat(fs_name, &sb) < 0) {
+         fprintf(stderr, "Failed in lstat with %s", fs_name);
+         return 0;
+     }
+
+     //проверяем fs_name. Если его тип не директория, то выдаем ошибку
+     if ((sb.st_mode & S_IFMT) != S_IFDIR) {
+         fprintf(stderr, "%s is not file system", fs_name);
+         return 0;
+     }
+
+     if (statvfs(fs_name, &sbfs) == -1) {
+         fprintf(stderr, "Failed in statvfs with %s", fs_name);
+         return 0;
+     }
+
+     if (strlen(file_name) != 0) {
+         if (lstat(file_name, &sb) < 0) {
+             fprintf(stderr, "Failed in lstat with %s", file_name);
+             return 0;
+         }
+     }
+     else {
+         return 0;
+     }
+
+     system_file_info(fs_name, &sbfs);
+
+     if ((sbfs.f_favail < 1) || ((long int) (sbfs.f_bavail * sbfs.f_bsize) < sb.st_size)) {
+        fprintf(stderr, "\nThe file %s doesn't fit to %s\n", file_name, fs_name);
+     }
+     else {
+       printf("\nThe file %s fits to %s\n", file_name, fs_name);
+     }
+     return 1;
+ }
